802d041de6639a8be28127e580628742e54a9e5c
payemo <jgenie97@gmail.com>
2023-01-15 00:40:00 +0200

    Add Color class for manipulating colors and class for managing a screen buffer

---
+++ ArcadeApp/ArcadeApp/src/Graphics/ScreenBuffer.cpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "ScreenBuffer.h"
+#include <SDL2/SDL.h>
+#include <cassert>
+
+ScreenBuffer::ScreenBuffer() : mSurface(nullptr)
+{
+}
+
+ScreenBuffer::ScreenBuffer(const ScreenBuffer& other)
+{
+	mSurface = SDL_CreateRGBSurfaceWithFormat(0, other.mSurface->w, other.mSurface->h, 0, other.mSurface->format->format);
+
+	SDL_BlitSurface(other.mSurface, nullptr, mSurface, nullptr);
+}
+
+ScreenBuffer::~ScreenBuffer()
+{
+	if (mSurface)
+	{
+		SDL_FreeSurface(mSurface);
+	}
+}
+
+ScreenBuffer& ScreenBuffer::operator=(const ScreenBuffer& other)
+{
+	if (this == &other)
+	{
+		return *this;
+	}
+
+	if (mSurface != nullptr)
+	{
+		SDL_FreeSurface(mSurface);
+		mSurface = nullptr;
+	}
+
+	if (other.mSurface != nullptr)
+	{
+		mSurface = SDL_CreateRGBSurfaceWithFormat(0, other.mSurface->w, other.mSurface->h, 0, other.mSurface->format->format);
+		SDL_BlitSurface(other.mSurface, nullptr, mSurface, nullptr);
+	}
+}
+
+void ScreenBuffer::Init(uint32_t format, uint32_t width, uint32_t height)
+{
+	mSurface = SDL_CreateRGBSurfaceWithFormat(0, width, height, 0, format);
+	Clear();
+}
+
+void ScreenBuffer::Clear(const Color& c)
+{
+	assert(mSurface);
+	if (mSurface)
+	{
+		SDL_FillRect(mSurface, nullptr, c.GetPixelColor());
+	}
+}
+
+void ScreenBuffer::SetPixel(const Color& color, int x, int y)
+{
+	assert(mSurface);
+	if (mSurface && (y < mSurface->h && y >= 0 && x >= 0 && x < mSurface->w))
+	{
+		SDL_LockSurface(mSurface);
+
+		uint32_t* pixels = (uint32_t*)mSurface->pixels;
+
+		size_t index = GetIndex(y, x);
+		pixels[index] = color.GetPixelColor();
+
+		SDL_UnlockSurface(mSurface);
+	}
+}
+
+uint32_t ScreenBuffer::GetIndex(int row, int col)
+{
+	assert(mSurface);
+	if (mSurface)
+	{
+		return row * mSurface->w + col;
+	}
+	return 0;
+}
--- ArcadeApp/ArcadeApp/src/main.cpp
+++ ArcadeApp/ArcadeApp/src/main.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
-#include "Vec2D.h"
-//#include "SDL2/SDL.h"
+#include "SDL2/SDL.h"
+
+#include "Color.h"
+#include "ScreenBuffer.h"
 
 const int SCREEN_WIDTH = 480;
 const int SCREEN_HEIGHT = 640;
 
-int main(int argc, char* args[])
-{
-	Vec2D v1(2, 5);
-	Vec2D v2(4, 3);
-
-	std::cout << "Magnitude of v1: " << v1.Mag() << std::endl;
-	std::cout << "Magnitude of v2: " << v2.Mag() << std::endl;
-	std::cout << "Distance between vectors is: " << v1.Distance(v2) << std::endl;
-	return 1;
-}
-
-/*
-int main()
+int main(int argc, char* argv[])
 {
 	if (SDL_Init(SDL_INIT_VIDEO))
 	{
 		std::cerr << "SDL_Init failed!" << std::endl;
 		return 1;
 	}
-
 
 	SDL_Window* window = SDL_CreateWindow("Arcade", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
 
@@ -33,6 +22,20 @@ int main()
 		std::cerr << "Could not create window." << std::endl;
 		return 1;
 	}
+
+	SDL_Surface* windowSurface = SDL_GetWindowSurface(window);
+	SDL_PixelFormat* pixFormat = windowSurface->format;
+
+	Color::InitColorFormat(pixFormat);
+
+	ScreenBuffer sb;
+	sb.Init(pixFormat->format, windowSurface->w, windowSurface->h);
+
+	sb.SetPixel(Color::Red(), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+
+	SDL_BlitSurface(sb.GetSurface(), nullptr, windowSurface, nullptr);
+
+	SDL_UpdateWindowSurface(window);
 
 	SDL_Event event;
 	bool running = true;
@@ -53,4 +56,4 @@ int main()
 	SDL_Quit();
 
 	return 0;
-} */
+} 
