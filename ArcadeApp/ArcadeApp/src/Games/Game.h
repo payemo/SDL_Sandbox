14d3e6d92b3c9b06070c6df50dd079cebe6af550
payemo <jgenie97@gmail.com>
2023-02-13 00:25:17 +0200

    Add scene class for all arcade games

---
+++ ArcadeApp/ArcadeApp/src/Games/Game.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <string>
+#include <stdint.h>
+
+class GameController;
+class Screen;
+
+class Game
+{
+public:
+	virtual ~Game() {}
+	virtual void Init(GameController& gameController) = 0;
+	virtual void Update(uint32_t dt) = 0;
+	virtual void Draw(Screen& screen) = 0;
+	virtual std::string& GetName() const = 0;
+};
