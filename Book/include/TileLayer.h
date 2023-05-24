d465ea70359c863b92637c020845957b191e1ca4
payemo <jgenie97@gmail.com>
2023-05-25 01:47:59 +0300

    Creating the TileLayer class

---
+++ Book/include/TileLayer.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <vector>
+#include "Layer.h"
+#include "Vector2D.h"
+
+struct Tileset;
+
+class TileLayer : public Layer
+{
+public:
+    TileLayer(int tileSize, const std::vector<Tileset>& tilesets);
+
+    virtual void Update() override;
+    virtual void Render() override;
+
+    inline void SetTileIds(const std::vector<std::vector<int>>& data) { m_tileIds = data; }
+    inline void SetTileSize(int tileSize) { m_tileSize = tileSize; }
+
+private:
+    const std::vector<Tileset>& m_tilesets;
+
+private:
+    int m_numColumns;
+    int m_numRows;
+    int m_tileSize;
+
+    Vector2D m_position;
+    Vector2D m_velocity;
+
+    std::vector<std::vector<int>> m_tileIds;
+};
---
+++ Book/src/TileLayer.cpp
@@ -0,0 +1,18 @@
+#include "TileLayer.h"
+#include "Level.h"
+
+TileLayer::TileLayer(int tileSize, const std::vector<Tileset>& tilesets)
+    :m_tileSize(tileSize), m_tilesets(tilesets)
+{
+
+}
+
+void TileLayer::Update()
+{
+
+}
+
+void TileLayer::Render()
+{
+
+}
